886ad4b1ea5940548c51ee599b8c4bb2b70bcc11
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-05 00:52:01 -0800

    Created sudo code

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,3 +3,16 @@ var APIKey = "35860f199216a9759f17e01b3000e2e2";
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // For example: https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 // Take out the bracket!
+
+// click action on search
+//      Call api
+//      fetch url
+//      add to a textContent
+
+
+// get current city day
+
+// get 5 more days 
+
+
+// 
