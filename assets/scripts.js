4c9444ca193e056f5a8af954bbfdce902e6e8043
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-07 18:29:24 -0800

    added variables

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,9 @@
 var APIKey = "35860f199216a9759f17e01b3000e2e2";
 var searchBTN = document.getElementById("search-BTN");
+
+var userInput = document.getElementById("user-input").value; // user's input of city name
+
+
 // Way to call city (by itself) by using name
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // For example: https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
@@ -14,8 +18,9 @@ function getWeatherFrom(cityName){
     })
     .then(function (data) {
         console.log ("Check the data: " + data);
-    });
 
+    });
+    console.log(weatherURL);
 }
 
 
@@ -24,5 +29,5 @@ function getWeatherFrom(cityName){
 // get 5 more days 
 
 // click action on search
-// searchBTN.addEventListener('click', getWeatherFrom);
-
+// searchBTN.addEventListener('click', getWeatherFrom(userInput));
+getWeatherFrom("London");
