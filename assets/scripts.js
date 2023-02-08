8cee9507e42b5ca9059473d9667674e90f6b6c37
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-07 18:01:17 -0800

    create a function to search data from city name

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,18 +1,20 @@
 var APIKey = "35860f199216a9759f17e01b3000e2e2";
+var searchBTN = document.getElementById("search-BTN");
 // Way to call city (by itself) by using name
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // For example: https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
 // Take out the bracket!
 
-// click action on search
-//      Call api
-//      fetch url
-//      add to a textContent
+function getWeatherFrom(cityName){
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName +"&appid={" + APIKey + "}";
+    
+}
 
 
 // get current city day
 
 // get 5 more days 
 
+// click action on search
+// searchBTN.addEventListener('click', getWeatherFrom);
 
-// 
