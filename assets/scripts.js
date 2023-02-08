8031609b840b92c7205827a02418b8f1d6ece700
JeanSuw <119563534+JeanSuw@users.noreply.github.com>
2023-02-08 03:52:06 -0800

    debugging user input

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,7 @@
 var APIKey = "35860f199216a9759f17e01b3000e2e2";
 var searchBTN = document.getElementById("search-BTN");
 
-var userInput = document.getElementById("user-input").value; // user's input of city name
+var userInput = document.getElementById("user-input"); // user's input of city name
 
 var cityNameDiv = document.getElementById("city-name");
 var todayDate = document.getElementById("today-date");
@@ -16,6 +16,7 @@ var todayHumidity = document.getElementById("today-humidity");
 // Take out the bracket!
 
 function getWeatherFrom(cityName){
+    console.log(weatherURL);
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName +"&appid=" + APIKey;
 
     fetch(weatherURL)
@@ -24,6 +25,8 @@ function getWeatherFrom(cityName){
     })
     .then(function (data) {
         //console.log ("Check the data: " + data.main.temp);
+        
+        // get current city day
         var cityVal = document.createElement('city-name');
         var today = new Date(data.dt*1000);
         var Fahrenheit = document.createElement('today-temp');
@@ -35,16 +38,18 @@ function getWeatherFrom(cityName){
         windSpeed.textContent = data.wind.speed + " MPH";
         humid.textContent = data.main.humidity + "%";
         
-
         cityNameDiv.appendChild(cityVal);
         todayTemp.appendChild(Fahrenheit);
         todayWind.appendChild(windSpeed);
         todayHumidity.appendChild(humid);
 
+        // get 5 more days 
+
     });
     console.log(weatherURL);
 }
-// ℉=((K-273.15)*1.8)+32
+
+// Helper method for temperature
 function convertToF(kelvin){
     total = ((kelvin-273.15)*1.8)+32;
     return total.toFixed(2);
@@ -52,10 +57,17 @@ function convertToF(kelvin){
 
 
 
-// get current city day
 
-// get 5 more days 
 
-// click action on search
-// searchBTN.addEventListener('click', getWeatherFrom(userInput));
-getWeatherFrom("London");
+
+
+// click to search something
+searchBTN.addEventListener('click', function (event) {
+    event.preventDefault();
+    getWeatherFrom(userInput.value); // Get input by calling a value
+}
+
+);
+
+// Testing 
+//getWeatherFrom("London");
